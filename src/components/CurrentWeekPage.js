8d4d023a7899e6b5e8befd4484fa6ff32bed5c02
William May <wjrm500@gmail.com>
2022-10-15 02:40:46 +0100

    Adding cursor point and arrow enlarge to arrows on current week page

--- src/components/CurrentWeekPage.js
+++ src/components/CurrentWeekPage.js
@@ -7,12 +7,16 @@ const CurrentWeekPage = ({ cwData, cwIndex, setCwIndex }) => {
   const rightArrowActive = cwIndex < cwData.length - 1
   return (
     <div id="currentWeekPage" className="page">
-      <div id="leftArrow" className={leftArrowActive ? 'arrow active' : 'arrow'}>
-        <FaArrowLeft onClick={() => leftArrowActive ? setCwIndex(cwIndex - 1) : ''} />
+      <div className="arrowContainer">
+        <div id="leftArrow" className={leftArrowActive ? 'arrow active' : 'arrow'}>
+          <FaArrowLeft onClick={() => leftArrowActive ? setCwIndex(cwIndex - 1) : ''} />
+        </div>
       </div>
       <ScoreTable cwData={cwData} cwIndex={cwIndex} />
-      <div id="rightArrow" className={rightArrowActive ? 'arrow active' : 'arrow'}>
-        <FaArrowRight onClick={() => rightArrowActive ? setCwIndex(cwIndex + 1): ''} />
+      <div className="arrowContainer">
+        <div id="rightArrow" className={rightArrowActive ? 'arrow active' : 'arrow'}>
+          <FaArrowRight onClick={() => rightArrowActive ? setCwIndex(cwIndex + 1): ''} />
+        </div>
       </div>
     </div>
   )
