c8a73390eb923a61658ee607b1109fbf7d4c7828
William May <wjrm500@gmail.com>
2022-11-03 22:22:50 +0800

    Wrapping login box HTML in form tags

--- src/components/LoginBox.js
+++ src/components/LoginBox.js
@@ -6,7 +6,8 @@ const LoginBox = ({ onLogin }) => {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const [loginIsLoading, setLoginIsLoading] = useState(false)
-    const onSubmit = () => {
+    const onSubmit = (e) => {
+        e.preventDefault()
         if (username == '' || password == '') {
             alert('Please enter both a username and a password')
             return
@@ -16,21 +17,23 @@ const LoginBox = ({ onLogin }) => {
     }
     return (
         <div id="loginBox">
-            <div className="loginBoxField">
-                <label>Username</label>
-                <input type="text" onChange={(e) => setUsername(e.target.value)} onKeyPress={(e) => e.key == 'Enter' ? onSubmit() : ''} />
-            </div>
-            <div className="loginBoxField">
-                <label>Password</label>
-                <input type="password" onChange={(e) => setPassword(e.target.value)} onKeyPress={(e) => e.key == 'Enter' ? onSubmit() : ''} />
-            </div>
-            <button id="loginButton">
-                {
-                    loginIsLoading
-                    ? <SpinningLoader />
-                    : <input type="submit" value="Submit" onClick={onSubmit} />
-                }
-            </button>
+            <form method="post" onSubmit={onSubmit}>
+                <div className="loginBoxField">
+                    <label>Username</label>
+                    <input type="text" onChange={(e) => setUsername(e.target.value)} onKeyPress={(e) => e.key == 'Enter' ? onSubmit() : ''} />
+                </div>
+                <div className="loginBoxField">
+                    <label>Password</label>
+                    <input type="password" onChange={(e) => setPassword(e.target.value)} onKeyPress={(e) => e.key == 'Enter' ? onSubmit() : ''} />
+                </div>
+                <button id="loginButton">
+                    {
+                        loginIsLoading
+                        ? <SpinningLoader />
+                        : <input type="submit" value="Submit" />
+                    }
+                </button>
+            </form>
         </div>
     )
 }
