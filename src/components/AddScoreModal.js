339ea4a6b5384e5c9eca984de8cdbf544a9cc067
William May <wjrm500@gmail.com>
2022-11-10 01:01:10 +0800

    Being more intelligent in the way score options are generated

--- src/components/AddScoreModal.js
+++ src/components/AddScoreModal.js
@@ -6,19 +6,16 @@ const AddScoreModal = ({ loggedInUser, addScore, setShowAddScoreModal }) => {
     addScore(dateToday, loggedInUser, event.target.value)
     setShowAddScoreModal(false)
   }
+  const options = [1, 2, 3, 4, 5, 6, 'Fail'].map(option => (
+    <option value={option}>{option}</option>
+  ))
   return (
     <div id="addScoreModal">
       <div>Add your score</div>
       <form>
         <select name="score" onChange={onChange}>
           <option value="" selected disabled>Select</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="8">Fail</option>
+          {options}
         </select>
       </form>
     </div>
