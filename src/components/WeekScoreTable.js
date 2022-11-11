e68a287e217f1b2d72cbb37cccc50f461bbe18a0
William May <wjrm500@gmail.com>
2022-11-11 09:57:25 +0800

    Initial attempt at tidying WeekScoreTable component by factoring out stuff from direct render into variables

--- src/components/WeekScoreTable.js
+++ src/components/WeekScoreTable.js
@@ -1,58 +1,51 @@
 import React from "react"
 
 const WeekScoreTable = ({weekData, weekIndex}) => {
+  const headerRow = <tr>
+    <th>Start date</th>
+    <th>Kate</th>
+    <th>Will</th>
+  </tr>
+  const dataRows = weekData[weekIndex].map(week => {
+    return (
+      <tr key={week.StartDate}>
+        <td>
+          {
+            new Date(
+              week.StartDate.slice(0, 4),
+              week.StartDate.slice(5, 7) - 1,
+              week.StartDate.slice(8, 10)
+            ).toLocaleString(
+              undefined, {
+                weekday: "short",
+                month: "short",
+                day: "2-digit"
+              }
+            )
+          }
+        </td>
+        <td>
+          {week.KateTotal}
+        </td>
+        <td>
+          {week.WillTotal}
+        </td>
+      </tr>
+    )
+  })
+  const summaryRow = <tr>
+    <td></td>
+    <td>{weekData[weekIndex].reduce((score, week) => score + week.KateTotal, 0)}</td>
+    <td>{weekData[weekIndex].reduce((score, week) => score + week.WillTotal, 0)}</td>
+  </tr>
   return (
     <table id="weekScoreTable" className="scoreTable" cellSpacing={0}>
       <thead>
-        <tr>
-          <th>Start date</th>
-          <th>Kate</th>
-          <th>Will</th>
-        </tr>
+        {headerRow}
       </thead>
       <tbody>
-        {
-          weekData[weekIndex].map(week => {
-            return (
-              <tr key={week.StartDate}>
-                <td>
-                  {
-                    new Date(
-                      week.StartDate.slice(0, 4),
-                      week.StartDate.slice(5, 7) - 1,
-                      week.StartDate.slice(8, 10)
-                    ).toLocaleString(
-                      undefined, {
-                        weekday: "short",
-                        month: "short",
-                        day: "2-digit"
-                      }
-                    )
-                  }
-                </td>
-                <td>
-                  {week.KateTotal}
-                </td>
-                <td>
-                  {week.WillTotal}
-                </td>
-              </tr>
-            )
-          })
-        }
-        <tr>
-          <td></td>
-          <td>
-            {
-              weekData[weekIndex].reduce((score, week) => score + week.KateTotal, 0)
-            }
-          </td>
-          <td>
-            {
-              weekData[weekIndex].reduce((score, week) => score + week.WillTotal, 0)
-            }
-          </td>
-        </tr>
+        {dataRows}
+        {summaryRow}
       </tbody>
     </table>
   )
