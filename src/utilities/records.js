fa4f14f9ee052354bd1176858cf2f415f395a784
William May <wjrm500@gmail.com>
2023-01-11 00:15:32 +0800

    Calculating most consecutive wins - initial implementation

--- src/utilities/records.js
+++ src/utilities/records.js
@@ -1,5 +1,30 @@
-const getRecords = (data) => {
-  
+const getMostConsecutiveWins = (data) => {
+  let previousWinner = null
+  let currentWinner = null
+  let streaks = []
+  for (let week of data) {
+    for (let day of week) {
+      if (day.Kate < day.Will) {
+        currentWinner = "Kate"
+      } else if (day.Will < day.Kate) {
+        currentWinner = "Will"
+      } else {
+        currentWinner = null
+      }
+      if (currentWinner) {
+        if (currentWinner != previousWinner) {
+          streaks.push([currentWinner, 1, day.Date])
+        } else if (currentWinner == previousWinner) {
+          let [currentStreak] = streaks.slice(-1)
+          currentStreak[1] += 1
+          currentStreak[2] = day.Date
+          streaks[streaks.length - 1] = currentStreak
+        }
+      }
+      previousWinner = currentWinner
+    }
+  }
+  return streaks
 }
 
-export default getRecords
+export {getMostConsecutiveWins}
