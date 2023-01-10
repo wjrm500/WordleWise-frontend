91bee0ba603d8b6afb64a682d6ea42d9d8a3b206
William May <wjrm500@gmail.com>
2023-01-11 00:38:14 +0800

    Adding table of consecutive wins to records page

--- src/components/RecordPage.js
+++ src/components/RecordPage.js
@@ -1,11 +1,31 @@
 import React from 'react'
-import getRecords from '../utilities/records'
+import { getWinStreaks } from '../utilities/records'
 
 const RecordPage = ({data}) => {
-  const records = getRecords(data)
+  const winStreaks = getWinStreaks(data)
+  const headerRow = (
+    <tr>
+      <th>#</th>
+      <th>Player</th>
+      <th>Num days</th>
+      <th>End date</th>
+    </tr>
+  )
+  const rows = winStreaks.map((streak, index) => (
+    <tr>
+      <td>{index + 1}</td>
+      <td>{streak.player}</td>
+      <td>{streak.numDays}</td>
+      <td>{streak.endDate}</td>
+    </tr>
+  ))
   return (
-    <div className="page">
-      RecordPage
+    <div className="page" style={{alignItems: 'center', flexDirection: 'column'}}>
+      <div style={{paddingBottom: '5px'}}>Most consecutive wins</div>
+      <table className="recordTable">
+        {headerRow}
+        {rows}
+      </table>
     </div>
   )
 }
