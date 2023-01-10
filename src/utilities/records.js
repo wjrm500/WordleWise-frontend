74410f75631c29bd4e40480f4b546dbaafbf0b45
William May <wjrm500@gmail.com>
2023-01-11 00:37:46 +0800

    Refactoring and renaming consecutive wins function

--- src/utilities/records.js
+++ src/utilities/records.js
@@ -1,30 +1,26 @@
-const getMostConsecutiveWins = (data) => {
+const getWinStreaks = (data) => {
   let previousWinner = null
-  let currentWinner = null
   let streaks = []
-  for (let week of data) {
-    for (let day of week) {
-      if (day.Kate < day.Will) {
-        currentWinner = "Kate"
-      } else if (day.Will < day.Kate) {
-        currentWinner = "Will"
-      } else {
-        currentWinner = null
-      }
-      if (currentWinner) {
-        if (currentWinner != previousWinner) {
-          streaks.push([currentWinner, 1, day.Date])
-        } else if (currentWinner == previousWinner) {
-          let [currentStreak] = streaks.slice(-1)
-          currentStreak[1] += 1
-          currentStreak[2] = day.Date
-          streaks[streaks.length - 1] = currentStreak
-        }
-      }
-      previousWinner = currentWinner
+  data.flat().forEach(day => {
+    let currentWinner = day.Kate < day.Will ? "Kate" : day.Will < day.Kate ? "Will" : null
+    if (currentWinner && currentWinner === previousWinner) {
+      let currentStreak = streaks.pop()
+      currentStreak.numDays++
+      currentStreak.endDate = day.Date
+      streaks.push(currentStreak)
+    } else if (currentWinner) {
+      streaks.push({player: currentWinner, numDays: 1, endDate: day.Date})
     }
-  }
-  return streaks
+    previousWinner = currentWinner
+  })
+  streaks.sort((a, b) => {
+    if (b.numDays !== a.numDays) {
+      return b.numDays - a.numDays
+    } else {
+      return new Date(a.endDate) - new Date(b.endDate)
+    }
+  })
+  return streaks.slice(0, 5)
 }
 
-export {getMostConsecutiveWins}
+export {getWinStreaks}
