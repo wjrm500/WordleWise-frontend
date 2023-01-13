5414b398176139d70866af4dad18fc5621994436
William May <wjrm500@gmail.com>
2023-01-13 11:28:57 +0800

    Fixing separate error related to add score and loggedInUser

--- src/components/AddScoreModal.js
+++ src/components/AddScoreModal.js
@@ -3,18 +3,18 @@ import React from "react"
 const AddScoreModal = ({loggedInUser, addScore, setShowAddScoreModal}) => {
   const onChange = (event) => {
     const dateToday = new Date().toLocaleDateString("en-GB", {timeZone: "Asia/Singapore"}).split("/").reverse().join("-")
-    addScore(dateToday, loggedInUser, event.target.value)
+    addScore(dateToday, loggedInUser.username, event.target.value)
     setShowAddScoreModal(false)
   }
   const options = [1, 2, 3, 4, 5, 6, "Fail"].map(option => (
     <option value={option == "Fail" ? 8 : option}>{option}</option>
   ))
   return (
-    <div id="addScoreModal" class="scoreModal">
+    <div id="addScoreModal" className="scoreModal">
       <div>Add your score</div>
       <form>
-        <select name="score" onChange={onChange}>
-          <option value="" selected disabled>Select</option>
+        <select name="score" onChange={onChange} value="">
+          <option value="" disabled>Select</option>
           {options}
         </select>
       </form>
