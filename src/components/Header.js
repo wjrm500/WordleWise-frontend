3134b733c64ff418b40d2e649f81eaefaaa61e08
William May <wjrm500@gmail.com>
2023-11-13 13:16:52 +0000

    Header improvements

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import React, { useState } from "react"
 import UpdateScoreButton from "./UpdateScoreButton"
 import UpdateScoreModal from "./UpdateScoreModal"
 import ModalOverlay from "./ModalOverlay"
+import logo from '../images/WordleTracker logo.png'
 
 const Header = ({loggedInUser, onLogout, addScore}) => {
   const [showUpdateScoreModal, setShowUpdateScoreModal] = useState(false)
@@ -18,6 +19,7 @@ const Header = ({loggedInUser, onLogout, addScore}) => {
           </>
         ) : ""
       }
+      <img src={logo} alt="Wordle Tracker Logo" style={{ height: '50px', marginRight: '10px' }} />
       <div>Welcome to <b>Wordle Tracker</b></div>
       {loggedInUser && loggedInUser.admin ? <UpdateScoreButton onClick={onUpdateScoreButtonClick} /> : ""}
       {loggedInUser ? <LogoutButton onLogout={onLogout} /> : ""}
--- src/components/LogOutButton.js
+++ src/components/LogOutButton.js
@@ -1,9 +1,11 @@
 import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 
 const LogoutButton = ({onLogout}) => {
   return (
     <button id="logoutButton" className="headerButton" onClick={onLogout}>
-      Log out
+      <FontAwesomeIcon icon={faSignOutAlt} />
     </button>
   )
 }
