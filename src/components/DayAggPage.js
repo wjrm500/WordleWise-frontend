137323050e0c97149983b8f6b4e93ee57ba2c65a
William May <wjrm500@gmail.com>
2023-11-22 09:29:37 +0000

    Arrow keys to switch weeks in Daily panel

--- src/components/DayAggPage.js
+++ src/components/DayAggPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import DayScoreTable from "./DayScoreTable"
 import AddScoreModal from "./AddScoreModal"
 import ModalOverlay from "./ModalOverlay"
@@ -7,6 +7,22 @@ import RightArrow from "./RightArrow"
 
 const DayAggPage = ({loggedInUser, addScore, data, dayIndex, setDayIndex}) => {
   const [showAddScoreModal, setShowAddScoreModal] = useState(false)
+
+  const handleKeyPress = (event) => {
+    if (event.keyCode === 37 && dayIndex > 0) { // Left arrow key pressed
+      setDayIndex(dayIndex - 1);
+    } else if (event.keyCode === 39 && dayIndex < data.length - 1) { // Right arrow key pressed
+      setDayIndex(dayIndex + 1);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyPress);
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [dayIndex, data.length, setDayIndex]);
+
   const onAddScoreButtonClick = () => setShowAddScoreModal(true)
   const onModalOverlayClick = () => setShowAddScoreModal(false)
   return (
