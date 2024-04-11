42f0ce69dc5fa51f1e7e5abae9c8523043579d72
William May <48405095+wjrm500@users.noreply.github.com>
2024-04-11 06:47:27 +0100

    Update Header.js to import LogOutButton with correct casing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import LogoutButton from "./LogoutButton"
+import LogoutButton from "./LogOutButton"
 import React, { useState } from "react"
 import UpdateScoreButton from "./UpdateScoreButton"
 import UpdateScoreModal from "./UpdateScoreModal"
