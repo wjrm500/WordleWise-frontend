f214f6f98077837c39d0b8e53f87a4816b4f1ea1
William May <wjrm500@gmail.com>
2024-06-16 17:34:46 +0100

    Fixing LogoutButton import in Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import LogoutButton from "./LogOutButton"
+import LogoutButton from "./LogoutButton"
 import React, { useState } from "react"
 import UpdateScoreButton from "./UpdateScoreButton"
 import UpdateScoreModal from "./UpdateScoreModal"
