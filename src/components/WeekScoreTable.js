f4554b777f8ae5411e83a7b87cd35f442cddb703
William May <wjrm500@gmail.com>
2025-01-18 10:56:41 +0000

    Colour code weekly scores green or red by who won

--- src/components/WeekScoreTable.js
+++ src/components/WeekScoreTable.js
@@ -8,22 +8,34 @@ const WeekScoreTable = ({weekData, onWeekRowClick}) => {
     <th>Kate</th>
     <th>Will</th>
   </tr>
+
   const dataRows = weekData.map(week => {
+    const kateWins = week.KateTotal < week.WillTotal;
+    const isTie = week.KateTotal === week.WillTotal;
+    const kateStyle = {
+      ...kateWins ? { color: 'green' } : (isTie ? {} : { color: 'red' })
+    };
+    const willStyle = {
+      ...kateWins ? { color: 'red' } : (isTie ? {} : { color: 'green' })
+    };
+
     return (
       <tr key={week.StartDate} onClick={() => onWeekRowClick(week.index)} className="clickableRow">
         <td>{week.index + 1}</td>
         <td>{week.StartOfWeek}</td>
-        <td className="scoreColumn">{week.KateTotal}</td>
-        <td className="scoreColumn">{week.WillTotal}</td>
+        <td className="scoreColumn" style={kateStyle}>{week.KateTotal}</td>
+        <td className="scoreColumn" style={willStyle}>{week.WillTotal}</td>
       </tr>
     )
   })
+
   const summaryRow = <tr style={{backgroundColor: "var(--blue-3)", color: "white"}}>
     <td></td>
     <td></td>
     <td className="scoreColumn">{weekData.reduce((score, week) => score + week.KateTotal, 0)}</td>
     <td className="scoreColumn">{weekData.reduce((score, week) => score + week.WillTotal, 0)}</td>
   </tr>
+
   return (
     <div style={{width: "100%"}}>
       <div className="tableContainer" style={{height: "200px"}}>
