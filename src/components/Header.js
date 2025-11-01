00071d39963b98c95d86d7c6f104407be49b89c1
William May <wjrm500@gmail.com>
2025-11-01 00:52:49 +0000

    Support custom page title

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,30 @@
 import LogoutButton from "./LogoutButton"
-import React, { useState } from "react"
+import React, { useState, useEffect, useContext } from "react"
 import UpdateScoreButton from "./UpdateScoreButton"
 import UpdateScoreModal from "./UpdateScoreModal"
 import PlayWordleButton from "./PlayWordleButton"
 import PlayWordleModal from "./PlayWordleModal"
 import ModalOverlay from "./ModalOverlay"
+import ServerAddrContext from "../contexts/ServerAddrContext"
 import logo from '../images/logo.png'
 
 const Header = ({loggedInUser, onLogout, addScore, users}) => {
   const [showUpdateScoreModal, setShowUpdateScoreModal] = useState(false)
   const [showPlayWordleModal, setShowPlayWordleModal] = useState(false)
+  const [title, setTitle] = useState(null)
+  const SERVER_ADDR = useContext(ServerAddrContext)
+  
+  useEffect(() => {
+    const fetchTitle = () => {
+      fetch(`${SERVER_ADDR}/getTitle`)
+        .then(r => r.json())
+        .then(data => setTitle(data.title))
+        .catch(() => {})
+    }
+    fetchTitle()
+    const interval = setInterval(fetchTitle, 60000)
+    return () => clearInterval(interval)
+  }, [SERVER_ADDR])
   
   const onUpdateScoreButtonClick = () => setShowUpdateScoreModal(true)
   const onPlayWordleButtonClick = () => setShowPlayWordleModal(true)
@@ -37,7 +52,7 @@ const Header = ({loggedInUser, onLogout, addScore, users}) => {
         ) : ""
       }
       <img src={logo} alt="WordleWise Logo" style={{ height: "50px" }} />
-      <div>Welcome to <b>WordleWise</b></div>
+      <div>{title ? (title === 'Welcome to WordleWise' ? <>Welcome to <b>WordleWise</b></> : title) : ''}</div>
       <div style={{ display: 'flex' }}>
         {loggedInUser ? <PlayWordleButton onClick={onPlayWordleButtonClick} /> : ""}
         {loggedInUser && loggedInUser.admin ? <UpdateScoreButton onClick={onUpdateScoreButtonClick} /> : ""}
